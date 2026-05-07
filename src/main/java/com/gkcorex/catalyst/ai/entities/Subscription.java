758bd42f8d10e0f32d73d83fa4a9c3948194c75c
Sri Venkata Ganesh Teja Potti <ganesh.potti27@gmail.com>
2026-05-07 22:26:37 +0530

    CAI-9 | feat: stripe backend handling part 2

--- src/main/java/com/gkcorex/catalyst/ai/entities/Subscription.java
+++ src/main/java/com/gkcorex/catalyst/ai/entities/Subscription.java
@@ -1,26 +1,37 @@
 package com.gkcorex.catalyst.ai.entities;
 
 import com.gkcorex.catalyst.ai.enums.SubscriptionStatus;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
 import java.time.Instant;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
 @Setter
+@Entity
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Subscription {
 
-  @Id Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  Long id;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(nullable = false, name = "user_id")
   User user;
 
+  @OneToMany(fetch = FetchType.LAZY)
+  @JoinColumn(nullable = false, name = "plan_id")
   Plan plan;
 
-  String subscriptionId;
+  String stripeSubscriptionId;
 
+  @Enumerated(value = EnumType.STRING)
   SubscriptionStatus status;
 
   Instant currentPeriodStart;
@@ -29,7 +40,9 @@ public class Subscription {
 
   Boolean cancelAtPeriodEnd = false;
 
+  @CreationTimestamp
   Instant createdAt;
 
+  @UpdateTimestamp
   Instant updateAt;
 }
